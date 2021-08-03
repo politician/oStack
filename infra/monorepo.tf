f8fb3204c06d85d25ee5e175eec66ec089663c79
Romain Barissat <romain-noreply@barissat.com>
2021-08-03 13:54:41 +0700

    Chore: Ensure forked PR's can merge

--- infra/monorepo.tf
+++ infra/monorepo.tf
@@ -59,7 +59,7 @@ module "ostack_monorepo" {
   auto_init              = false
   branch_delete_on_merge = true
   branch_protection      = true
-  branch_status_checks   = ["Passed all CI tests", "Terraform Cloud/olivr/oStack"]
+  branch_status_checks   = ["Passed all CI tests"]
   has_projects           = false
   has_wiki               = false
   homepage_url           = "https://oStack.io"
